90de7d22f12d7ff59360f015ae1d9e7a2b62ca93
Dan Shai <danshai2013@gmail.com>
2018-09-21 08:52:41 +0300

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-dfdc081f7751cb41dc3d.js"
+    "url": "webpack-runtime-176ccd2cb0833371c713.js"
   },
   {
     "url": "app-63155a19361e9b63c681.js"
@@ -36,17 +36,17 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "95defdcd84f74b01d643bc5c0f93728d"
+    "revision": "68edc7a26a2910c5a8074d69cd1f397b"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "da910aaf8b518a42a73baee5d03ea03e"
+    "revision": "98fbb0eb9299a9b8cb093717a29046f8"
   },
   {
-    "url": "component---src-pages-index-js.d61752646c24db75e0e6.css"
+    "url": "component---src-pages-index-js.cac5f8c446ed8ec3b662.css"
   },
   {
-    "url": "component---src-pages-index-js-0814fbab94f860ad318b.js"
+    "url": "component---src-pages-index-js-dbb394dfdaf5b967d128.js"
   },
   {
     "url": "1-1ade2c674151b0a1c1b3.js"
@@ -59,10 +59,10 @@ self.__precacheManifest = [
     "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
   },
   {
-    "url": "component---src-pages-404-js.d61752646c24db75e0e6.css"
+    "url": "component---src-pages-404-js.cac5f8c446ed8ec3b662.css"
   },
   {
-    "url": "component---src-pages-404-js-c9b4d86f6d43b3524696.js"
+    "url": "component---src-pages-404-js-a5e5f5b3473c6b5e4a06.js"
   },
   {
     "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
